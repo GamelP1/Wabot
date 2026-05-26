34741600a26939a67a0acbb0a48f9e414fae19f7
GHamelp1 <gamelalves78@gmail.com>
2026-05-26 15:54:01 -0300

    actual

--- tts.js
+++ tts.js
@@ -1,16 +1,30 @@
 const gTTS = require('node-gtts')('pt');
 const fs = require('fs');
 const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
 
 async function textToSpeech(texto) {
     return new Promise((resolve, reject) => {
-        const caminho = path.join(__dirname, `tts_${Date.now()}.mp3`);
+        const mp3 = path.join(__dirname, `tts_${Date.now()}.mp3`);
+        const ogg = mp3.replace('.mp3', '.ogg');
+
         const stream = gTTS.stream(texto);
-        const writer = fs.createWriteStream(caminho);
+        const writer = fs.createWriteStream(mp3);
 
         stream.pipe(writer);
 
-        writer.on('finish', () => resolve(caminho));
+        writer.on('finish', () => {
+            ffmpeg(mp3)
+                .audioCodec('libopus')
+                .format('ogg')
+                .on('end', () => {
+                    fs.unlinkSync(mp3); // apaga o mp3
+                    resolve(ogg);
+                })
+                .on('error', reject)
+                .save(ogg);
+        });
+
         writer.on('error', reject);
         stream.on('error', reject);
     });
